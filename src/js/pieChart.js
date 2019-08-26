2fc4aa17278ce2abb854ed74588965abdfcc9418
zionHuang <hjszion@gmail.com>
2019-08-26 13:09:44 -0700

    updated two-value

--- src/js/pieChart.js
+++ src/js/pieChart.js
@@ -11,6 +11,7 @@ var map01 = echarts.init(document.getElementById('m-graph-wraper-t'));
 //r-content
 var Stacking01 = echarts.init(document.getElementById('r-graph-wraper-t'));
 var twoValue01 = echarts.init(document.getElementById('r-graph-wraper-b-m'));
+var mixedBar01 = echarts.init(document.getElementById('r-graph-wraper-b-b'));
 
 //l-content-chart
 //scatter-map
@@ -429,7 +430,6 @@ Stacking01.setOption({
 //r-graph-wraper-b-m
 //twoValue
 var data = [];
-
 for (var i = 0; i <= 360; i++) {
     var t = i / 180 * Math.PI;
     var r = Math.sin(2 * t) * Math.cos(2 * t);
@@ -466,4 +466,75 @@ twoValue01.setOption({
         data: data
     }],
     animationDuration: 2000
+});
+
+//r-graph-wraper-b-b
+//mix-bar
+mixedBar01.setOption({
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            crossStyle: {
+                color: '#999'
+            }
+        }
+    },
+    toolbox: {
+        feature: {
+            // dataView: {show: true, readOnly: false},
+            // magicType: {show: true, type: ['line', 'bar']},
+            // restore: {show: true},
+            saveAsImage: {show: true}
+        }
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+            axisPointer: {
+                type: 'shadow'
+            }
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value',
+            name: '水量',
+            min: 0,
+            max: 250,
+            interval: 50,
+            axisLabel: {
+                formatter: '{value} ml'
+            }
+        },
+        {
+            type: 'value',
+            name: '温度',
+            min: 0,
+            max: 25,
+            interval: 5,
+            axisLabel: {
+                formatter: '{value} °C'
+            }
+        }
+    ],
+    series: [
+        {
+            name:'蒸发量',
+            type:'bar',
+            data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+        },
+        {
+            name:'降水量',
+            type:'bar',
+            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+        },
+        {
+            name:'平均温度',
+            type:'line',
+            yAxisIndex: 1,
+            data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+        }
+    ]
 });
